746ee6dbaca45588caee1be92135a31b35249f32
ROllerozxa <rollerozxa@voxelmanip.se>
2024-11-08 21:52:59 +0100

    `minetest` -> `core`

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
 
-minetest.override_item("", {
+core.override_item("", {
 	on_place = function(itemstack, placer, pointed_thing)
-		if minetest.is_creative_enabled(placer:get_player_name()) then
-			local pointed_node = minetest.get_node(pointed_thing.under)
+		if core.is_creative_enabled(placer:get_player_name()) then
+			local pointed_node = core.get_node(pointed_thing.under)
 			return pointed_node
 		end
 	end
